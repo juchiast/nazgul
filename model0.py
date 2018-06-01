4ed6fd31d71f891dd1f3098e0286d63b2f895adc
Do Duy <juchiast@gmail.com>
2018-06-01 20:41:34 +0700

    refactor

--- model0.py
+++ model0.py
@@ -1,4 +1,3 @@
-
 from __future__ import print_function
 import argparse
 import torch
@@ -6,6 +5,8 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 from torchvision import datasets, transforms
+
+import sys
 
 class Net(nn.Module):
     def __init__(self):
@@ -56,56 +57,53 @@ def test(args, model, device, test_loader):
         test_loss, correct, len(test_loader.dataset),
         100. * correct / len(test_loader.dataset)))
 
+class Args:
+    def __init__(self):
+        self.no_cuda = False
+        self.seed = 1
+        self.batch_size = 64
+        self.test_batch_size = 1000
+        self.epochs = 20
+        self.lr = 0.01
+        self.log_interval = 10
+        self.momentum = 0.5
+        self.save_path = './saved/model0'
+        self.data_path = './data_mnist'
+
 def main():
-    # Training settings
-    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
-    parser.add_argument('--batch-size', type=int, default=64, metavar='N',
-                        help='input batch size for training (default: 64)')
-    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
-                        help='input batch size for testing (default: 1000)')
-    parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                        help='number of epochs to train (default: 10)')
-    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
-                        help='learning rate (default: 0.01)')
-    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                        help='SGD momentum (default: 0.5)')
-    parser.add_argument('--no-cuda', action='store_true', default=False,
-                        help='disables CUDA training')
-    parser.add_argument('--seed', type=int, default=1, metavar='S',
-                        help='random seed (default: 1)')
-    parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                        help='how many batches to wait before logging training status')
-    args = parser.parse_args()
-    use_cuda = not args.no_cuda and torch.cuda.is_available()
+    args = Args()
 
     torch.manual_seed(args.seed)
 
+    use_cuda = not args.no_cuda and torch.cuda.is_available()
     device = torch.device("cuda" if use_cuda else "cpu")
-
     kwargs = {'num_workers': 1, 'pin_memory': True} if use_cuda else {}
-    train_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data_mnist', train=True, download=True,
-                       transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.batch_size, shuffle=True, **kwargs)
-    test_loader = torch.utils.data.DataLoader(
-        datasets.MNIST('./data_mnist', train=False, transform=transforms.Compose([
-                           transforms.ToTensor(),
-                           transforms.Normalize((0.1307,), (0.3081,))
-                       ])),
-        batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
+    transformer = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize((0.1307,), (0.3081,))
+    ])
 
     model = Net().to(device)
     optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
 
-    for epoch in range(1, args.epochs + 1):
-        train(args, model, device, train_loader, optimizer, epoch)
-        test(args, model, device, test_loader)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(args.data_path, train=False, transform=transformer),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
 
-    # torch.save(model, "./saved/model0")
+    if len(sys.argv) > 1 and sys.argv[1] == 'test':
+        model.load_state_dict(torch.load(args.save_path))
+        test(args, model, device, test_loader)
+    else:
+        train_loader = torch.utils.data.DataLoader(
+            datasets.MNIST(args.data_path, train=True, transform=transformer),
+            batch_size=args.batch_size, shuffle=True, **kwargs)
+
+        for epoch in range(args.epochs):
+            train(args, model, device, train_loader, optimizer, epoch)
+            test(args, model, device, test_loader)
+
+        torch.save(model.state_dict(), args.save_path)
 
 
 if __name__ == '__main__':
